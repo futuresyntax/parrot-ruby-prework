d80f2a2a1fdd4c7f8ae84547b51748b48ebf96b5
FutureScript <fernandobaxter1@gmail.com>
2018-11-09 12:06:58 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,13 +1,9 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(squawk)
-  puts squawk
-  return
-end 
-# def parrot(phrase = "Squawk!")
-#   return phrase
-# end
+def parrot(phrase = "Squawk!")
+   return phrase
+end
 
 
 # def meal_choice(meal_choice = "meat")
