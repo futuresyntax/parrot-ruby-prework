05e1549ffce0ab451c608473d774c4fb035011a4
FutureScript <fernandobaxter1@gmail.com>
2018-11-09 11:55:24 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,9 +1,9 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-<<<<<<< HEAD
-def parrot( phrase = )
-=======
-def parrot_ruby
-  
 
->>>>>>> 4b85bdbbccf6a118776296d14c988ec10a079fc8
+def parrot(phrase = "Squak")
+  returns ("phase")
+  puts("phrase")
+end
+  
+  
